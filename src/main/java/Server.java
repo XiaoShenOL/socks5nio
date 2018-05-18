f88d0c0b3fe0d8e6d6f728ddd49447b4e6bbf19e
gorbatov <gorbatov.eg@gmail.com>
2018-05-18 13:27:15 +0300

    Socks5 server with nio

---
+++ src/main/java/Server.java
@@ -0,0 +1,87 @@
+import stages.AuthMethodStage;
+import stages.IStage;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class Server {
+    private static Properties properties = new Properties();
+    private static String serverPortConfigName = "server.port";
+
+    public static void main(String[] args) {
+        Map<SocketChannel, IStage> map = new HashMap<>();
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open();
+             Selector selector = Selector.open()){
+            properties.load(ClassLoader.getSystemResourceAsStream("configuration.properties"));
+            serverChannel.configureBlocking(false);
+            serverChannel.bind(new InetSocketAddress(Integer.parseInt(properties.getProperty(serverPortConfigName))));
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            while (true) {
+                selector.select();
+                Set<SelectionKey> keys = selector.selectedKeys();
+                if (keys.isEmpty()) {
+                    continue;
+                }
+                keys.removeIf(key -> {
+                    if (!key.isValid()) {
+                        return true;
+                    }
+                    if (key.isAcceptable()) {
+                        return accept(map, key);
+                    }
+                    if (key.isReadable() || key.isConnectable()) {
+                        IStage stage = map.get(key.channel()).proceed(selector, map);
+                        if (stage == null) {
+                            closeChannel((SocketChannel) key.channel());
+                            return true;
+                        }
+                        return true;
+                    }
+                    return true;
+                });
+                map.keySet().removeIf(channel -> !channel.isOpen());
+            }
+
+        } catch (IOException e) {
+            LogUtils.logException("Unexpected error", e);
+        }
+    }
+
+    private static boolean accept(Map<SocketChannel, IStage> map, SelectionKey key) {
+        ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+        SocketChannel channel = null;
+        try {
+            System.out.print("Accepting ");
+            channel = serverChannel.accept();
+            System.out.print(channel.getRemoteAddress());
+            channel.configureBlocking(false);
+            channel.register(key.selector(), SelectionKey.OP_READ);
+            map.put(channel, new AuthMethodStage(channel));
+            System.out.println(" is accepted");
+        } catch (IOException e) {
+            System.out.println(" is not accepted");
+            LogUtils.logException("Failed to process channel " + channel, e);
+            if (channel != null) {
+                closeChannel(channel);
+            }
+        }
+        return true;
+    }
+
+    private static void closeChannel(SocketChannel channel) {
+        try {
+            System.out.println("Closing channel " + channel.getRemoteAddress());
+            channel.close();
+        } catch (IOException e) {
+            System.err.println("Failed to close channel " + channel);
+        }
+    }
+}
---
+++ src/main/java/stages/IStage.java
@@ -0,0 +1,9 @@
+package stages;
+
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+
+public interface IStage {
+    public IStage proceed(Selector selector, Map<SocketChannel, IStage> map);
+}
